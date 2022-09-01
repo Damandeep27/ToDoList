d08599f15f0c0b1d88bb4e59e31885d55619e076
Daman <71846343+Damandeep27@users.noreply.github.com>
2022-09-01 14:36:14 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = props => {
 
        
        <h1>My Todo List</h1>
-         <p style={{color: "white"; text-transform:"uppercase", font-weight:"bold"}}><a href="https://youtu.be/qBnPCOowjQs">Watch Demo</a></p>
+         <p style={{color: "white", text-transform:"uppercase", font-weight:"bold"}}><a href="https://youtu.be/qBnPCOowjQs">Watch Demo</a></p>
 
 
         <div className='background'>
